9a59dfe7d7ba35fe4ddb334b3b353c3d7d669c18
BladeanMericle <mericle.post@gmail.com>
2021-09-24 10:11:58 +0900

    ACPのワンタイムAPPを取得する関数を作成しました。

---
+++ lambda/acp/index.js
@@ -0,0 +1,22 @@
+const env = process.env;
+const serviceId = env.SERVICE_ID;
+const servicePassword = env.SERVICE_PASSWORD;
+const url = 'https://acp-api.amivoice.com/issue_service_authorization?sid=' + serviceId + '&spw=' + servicePassword;
+const https = require('https');
+
+exports.handler = async (event) => {
+    const promise = new Promise(function(resolve, reject) {
+        https.get(url, (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+            response.on('end', () => {
+                resolve(data);
+            });            
+        }).on('error', (e) => {
+            reject(new Error(e));
+        });
+    });
+    return promise
+};
